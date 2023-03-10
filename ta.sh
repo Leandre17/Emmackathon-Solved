bfbf4855e09abdef1dee7bf19274148cde48f3e6
Leandre17 <leandre.godet@epitech.eu>
2023-03-10 13:23:58 +0100

    first commit

--- ta.sh
+++ ta.sh
@@ -12,7 +12,7 @@ ls
 ls "../$3/Ex0$2/"
 ls "Ex0$2/"
 
-../$3/Ex0$2/$1 > $STUDENT_OUTPUT
-Ex0$2/$1 > $MY_OUTPUT
+./$3/Ex0$2/$1 > $STUDENT_OUTPUT
+./solution/Ex0$2/$1 > $MY_OUTPUT
 
 diff $STUDENT_OUTPUT $MY_OUTPUT
