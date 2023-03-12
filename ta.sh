0ac555918ad62aa2944614340d83699da8dc024c
Leandre17 <leandre.godet@epitech.eu>
2023-03-12 11:36:05 +0100

    c

--- ta.sh
+++ ta.sh
@@ -19,6 +19,8 @@ diff $STUDENT_OUTPUT $MY_OUTPUT
 while IFS='\n' read -ra ADDR; do
     cat sujet/Ex0$2/input$ADDR | ./$3/Ex0$2/$1 > $STUDENT_OUTPUT
     cat sujet/Ex0$2/input$ADDR | ./solution/Ex0$2/$1 > $MY_OUTPUT
+    cat $STUDENT_OUTPUT
+    cat $MY_OUTPUT
     diff $STUDENT_OUTPUT $MY_OUTPUT >> differ
 done <<< "$INPUT"
 
