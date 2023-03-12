b9d04789d7aec96260a99df6bed9a2e2ebb85903
Leandre17 <leandre.godet@epitech.eu>
2023-03-12 13:52:46 +0100

    c

--- ta.sh
+++ ta.sh
@@ -5,7 +5,7 @@ STUDENT_OUTPUT="./student_output"
 MY_OUTPUT="./my_output"
 TEST_NUMBER=1
 INPUT=$(ls sujet/Ex0$2/ | grep "input" | cut -c 6-)
-
+STATUS="/var/jenkins_home/workspace/status/$4"
 if [ $(ls ./$3/Ex0$2/$1 | wc -l) -eq 0 ]; then 
     echo -e "\nDelivery error :(\n"
     exit 1
@@ -20,8 +20,10 @@ while IFS='\n' read -ra ADDR; do
     if [ $(diff $STUDENT_OUTPUT $MY_OUTPUT | wc -l) -ne 0 ]; then 
         echo -e "\nIn ex0$2:\t TEST $TEST_NUMBER FAILLED:"
         diff $STUDENT_OUTPUT $MY_OUTPUT
+        echo "ex0$2 Fail at test number $TEST_NUMBER" >> $STATUS
         exit 84;
     fi;
     TEST_NUMBER=$((TEST_NUMBER + 1))
 done <<< "$INPUT"
 
+echo "ex0$2 SUCCEED" >> $STATUS
