d2208b6904d9e856b3cdaedacacfc3c683d62708
Leandre17 <leandre.godet@epitech.eu>
2023-03-12 11:30:01 +0100

    c

--- ta.sh
+++ ta.sh
@@ -8,17 +8,18 @@ STUDENT_OUTPUT="./student_output"
 EXEC=$1
 MY_OUTPUT="./my_output"
 
-INPUT=$(ls sujet/Ex0$2/ | grep "input" | cut -c 6-8)
-
-echo "input = $INPUT"
+INPUT=$(ls sujet/Ex0$2/ | grep "input" | cut -c 6-)
 
 chmod +x ./$3/Ex0$2/$1
 chmod +x ./solution/Ex0$2/$1
 
-./$3/Ex0$2/$1 > $STUDENT_OUTPUT
-./solution/Ex0$2/$1 > $MY_OUTPUT
 
 diff $STUDENT_OUTPUT $MY_OUTPUT
 
-#while IFS='\n' read -ra ADDR; do
-#done <<< "$ALL_CONTAINERS"
+while IFS='\n' read -ra ADDR; do
+    cat sujet/Ex0$2/input$ADDR | ./$3/Ex0$2/$1 > $STUDENT_OUTPUT
+    cat sujet/Ex0$2/input$ADDR | ./solution/Ex0$2/$1 > $MY_OUTPUT
+    diff $STUDENT_OUTPUT $MY_OUTPUT >> differ
+done <<< "$INPUT"
+
+cat differ
