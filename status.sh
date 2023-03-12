a9abc043bf1bfb678d85e128df2349c637aa8f74
Leandre17 <leandre.godet@epitech.eu>
2023-03-12 14:17:42 +0100

    c

--- status.sh
+++ status.sh
@@ -5,7 +5,8 @@ mkdir -p /var/jenkins_home/workspace/status/old/
 
 echo $INPUT
 while IFS='\n' read -ra ADDR; do
-	echo -e "\n\n$ADDR :"
+    if [[ "$ADDR" == "" ]] ; then continue; fi
+    echo -e "\n\n$ADDR :"
     cat /var/jenkins_home/workspace/status/$ADDR
     echo -e "\n\n"
     mv /var/jenkins_home/workspace/status/$ADDR /var/jenkins_home/workspace/status/old/${ADDR}_$BUILD_ID
