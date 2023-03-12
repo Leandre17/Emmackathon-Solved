21fc90c3cde186f72db27950c74fe54915b5d8dc
Leandre17 <leandre.godet@epitech.eu>
2023-03-12 12:03:24 +0100

    c

--- ta.sh
+++ ta.sh
@@ -16,12 +16,15 @@ chmod +x ./solution/Ex0$2/$1
 
 diff $STUDENT_OUTPUT $MY_OUTPUT
 
+TEST_NUMBER=1
+
 while IFS='\n' read -ra ADDR; do
     cat sujet/Ex0$2/input$ADDR | ./$3/Ex0$2/$1 > $STUDENT_OUTPUT
     cat sujet/Ex0$2/input$ADDR | ./solution/Ex0$2/$1 > $MY_OUTPUT
-    cat $STUDENT_OUTPUT
-    cat $MY_OUTPUT
-    diff $STUDENT_OUTPUT $MY_OUTPUT >> differ
+    if [ $(diff $STUDENT_OUTPUT $MY_OUTPUT | wc -l) -ne 0 ]; then 
+        echo -e "TEST $TEST_NUMBER FAILLED:"
+        diff $STUDENT_OUTPUT $MY_OUTPUT
+        exit 84;
+    TEST_NUMBER=$((TEST_NUMBER + 1))
 done <<< "$INPUT"
 
-cat differ
